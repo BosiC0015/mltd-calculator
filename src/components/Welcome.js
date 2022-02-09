8a3c40352e8c8e9b2ec233da68ce3a560fe6f41b
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-09 13:59:42 -0500

    complete welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 
+const imgs = [
+  'https://imas.gamedbs.jp/mlth/image/chara/loading/1626621122045_5stwjvyf.png',
+  'https://imas.gamedbs.jp/mlth/image/chara/loading/1626621120029_s0peziy7.png',
+  'https://imas.gamedbs.jp/mlth/image/chara/loading/1626621122044_9lbu2dca.png'
+];
+
+const getRandom = (num) => {
+  return Math.floor(Math.random() * num);
+}
+
 export default function Welcome(props) {
   return (
-    <div>Welcome</div>
+    <div className="main-container">
+      <span className="title">Welcome to MLTD Calculator</span>
+      <img className="hpimg" src={imgs[getRandom(3)]} />
+    </div>
   )
 }
