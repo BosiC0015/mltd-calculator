cffea0f4cda5f9d4eef6429dda387dc030d5403c
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-09 14:26:24 -0500

    complete pstour event page

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,11 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import ItemsQty from "./ItemsQty";
+import TourResult from "./TourResult";
 import "./styles.scss";
 
 
 export default function Tour(props) {
+  const [currentPoint, setCurrentPoint] = useState('');
+  const [targetPoint, setTargetPoint] = useState('');
+  const [eventItems, setEventItems] = useState('');
+  const [itemConsume, setItemConsume] = useState(1)
+  const [loadResults, setLoadResults] = useState(false);
+
+  const difference = targetPoint - currentPoint;
+
+  const eventSongPlays = (items) => {
+    if (items === 3) {
+      return difference / 2160; // use Math.ceil() for round
+    } else if (items === 2) {
+      return difference / 1440;
+    } else if (items === 1) {
+      return difference / 720;
+    }
+  }
+  
+  const clear = () => {
+    setCurrentPoint('');
+    setTargetPoint('');
+    setEventItems('');
+    setLoadResults(false);
+  }
+
+
   return (
-    <div>
-      <span>Platinum Star Tour Event Calculator</span>
+    <div className="main-container">
+      <span className="title">Platinum Star Tour Event Calculator</span>
+      <form
+        id="pstour-calc"
+        autoComplete="off"
+        onKeyDown={e => e.key === 'Enter' && e.preventDefault()}
+        onSubmit={e => e.preventDefault()}
+      >
+        <div className="form-input">
+            <label>Current Point</label>
+            <input
+              className="form-input__input"
+              placeholder="0"
+              value={currentPoint}
+              onChange={(event) => setCurrentPoint(event.target.value)}
+            />
+        </div>
+        <div className="form-input">
+          <label>Target Point</label>
+          <input
+            className="form-input__input"
+            placeholder="0"
+            value={targetPoint}
+            onChange={(event) => setTargetPoint(event.target.value)}
+          />
+        </div>
+        <div className="form-input">
+          <label>Event Items</label>
+          <input
+            className="form-input__input"
+            placeholder="0"
+            value={eventItems}
+            onChange={(event) => setEventItems(event.target.value)}
+          />
+        </div>
+        <div className="event-item-qty">
+          <label>How many event items do you use:</label>
+          <div className="event-item-qty-buttons">
+            <ItemsQty item={1} setItem={() => setItemConsume(1)} />
+            <ItemsQty item={2} setItem={() => setItemConsume(2)} />
+            <ItemsQty item={3} setItem={() => setItemConsume(3)} />
+          </div>
+        </div>
+        <div className="buttons">
+          <button className="buttons-clear" onClick={clear}>Clear</button>
+          <button className="buttons-calc" onClick={() => setLoadResults(true)}>Calculate</button>
+        </div>
+        <TourResult
+          load={loadResults}
+          difference={difference}
+          eventSongPlays={eventSongPlays(itemConsume)}
+        />
+      </form>
     </div>
   );
 };
---
+++ src/components/TourResult.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.scss";
+
+
+export default function TourResult(props) {
+  if (props.load === false) {
+    return (<div className="result"></div>);
+  }
+  return (
+    <div className="result">
+      <p>(5 * event songs)</p>
+      <p>You still have {props.difference} points to go.</p>
+      <p>You still need to play {props.eventSongPlays} times event song.</p>
+    </div>
+  )
+}
