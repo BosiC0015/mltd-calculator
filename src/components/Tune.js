42f2c4b67cfbc8cb435fd6fdb4163869cdaccf57
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-09 15:13:09 -0500

    temporary pstune event page

--- src/components/Tune.js
+++ src/components/Tune.js
@@ -4,8 +4,10 @@ import "./styles.scss";
 
 export default function Tune(props) {
   return (
-    <div>
-      <span>Platinum Star Tune Event Calculator</span>
+    <div className="main-container">
+      <span className="title">Platinum Star Tune Event Calculator</span>
+      <img src="https://imas.gamedbs.jp/mlth/image/chara/loading/1626621123052_so3narhv.png" />
+      <p className="title">To Be Developed...(Waiting for next tune event)</p>
     </div>
   );
 };
