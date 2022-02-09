041a375d94b91e10d9bac9f9ab1ac1bb34ca5c22
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-08 21:38:34 -0500

    create page for pstour events

---
+++ src/components/Tour.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.scss";
+
+
+export default function Tour(props) {
+  return (
+    <div>
+      <span>Platinum Star Tour Event Calculator</span>
+    </div>
+  );
+};
