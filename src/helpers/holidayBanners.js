6bf622a08cd7439d71d598f99aa73aab56ae9176
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-10 15:38:04 -0500

    add helper files

---
+++ src/helpers/holidayBanners.js
@@ -0,0 +1,46 @@
+const valentine = {
+  2019: 'https://imas.gamedbs.jp/mlth/image/chara/other/1550122672001_9yrgeiuw.jpg',
+  2020: 'https://imas.gamedbs.jp/mlth/image/chara/other/1592368427001_w5hd6n9b.png',
+  2021: 'https://imas.gamedbs.jp/mlth/image/chara/other/1613229890001_zf8vb06t.png',
+};
+
+const mlAnniversaries = {
+  7: 'https://imas.gamedbs.jp/mlth/image/chara/other/1582784883001_ug7isp42_2.jpg',
+  8: 'https://imas.gamedbs.jp/mlth/image/chara/other/1623910460002_7l3p0cx8_1.png'
+};
+
+const whiteDays = {
+  2019: 'https://imas.gamedbs.jp/mlth/image/chara/other/1552539869002_zkmwbxyt.jpg',
+  2020: 'https://imas.gamedbs.jp/mlth/image/chara/other/1584141024001_waqzy8u6_2.png',
+  2021: 'https://imas.gamedbs.jp/mlth/image/chara/other/1623910459001_85hersac_1.png'
+};
+
+const anniversaries = {
+  '2nd3d': 'https://imas.gamedbs.jp/mlth/image/chara/other/1561616180001_bye7vo5m.png',
+  '2nd2d': 'https://imas.gamedbs.jp/mlth/image/chara/other/1561616200001_b6pvehk0.png',
+  '3rd3d': 'https://imas.gamedbs.jp/mlth/image/chara/other/1593097554001_jn0za1ie.png',
+  '3rd2d': 'https://imas.gamedbs.jp/mlth/image/chara/other/1593319698001_yzwag2tr.png',
+  '3rd1d': 'https://imas.gamedbs.jp/mlth/image/chara/other/1593319950001_bvqyi689.png',
+  '4th4d': 'https://imas.gamedbs.jp/mlth/image/chara/other/1624547659001_w651hzbj.png',
+  '4th3d': 'https://imas.gamedbs.jp/mlth/image/chara/other/1624633728001_8nbiue3h.png',
+  '4th2d': 'https://imas.gamedbs.jp/mlth/image/chara/other/1624719688001_widr3fa4.png',
+  '4th1d': 'https://imas.gamedbs.jp/mlth/image/chara/other/1624806534001_14tbj0ih.png'
+};
+
+const imasAnniversaries = {
+  '15th2d': 'https://imas.gamedbs.jp/mlth/image/chara/other/1595516763001_z87yisr1_1.png',
+  '15th': 'https://imas.gamedbs.jp/mlth/image/chara/other/1595691318001_6h4jgx7a_1.png'
+};
+
+const kanshasai = {
+  '2018': 'https://imas.gamedbs.jp/mlth/image/chara/other/1555569698002_r9q3hpaw.jpg',
+  '2019~2020': 'https://imas.gamedbs.jp/mlth/image/chara/other/1585290381001_oept6yg0_0.png',
+};
+
+const chiristmas = {
+  2019: 'https://imas.gamedbs.jp/mlth/image/chara/other/1584142196001_m0ybnj19_2.png',
+  2020: 'https://imas.gamedbs.jp/mlth/image/chara/other/1608822227001_0ykoibhf_2.png'
+};
+
+
+module.exports = { valentine, mlAnniversaries, whiteDays, anniversaries, imasAnniversaries, kanshasai, chiristmas }
