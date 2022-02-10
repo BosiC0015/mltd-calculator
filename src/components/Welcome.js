e39f168c92f9aef7537ac17dfaf4aefa7da386b4
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-10 17:55:06 -0500

    add modes for special days banner components

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,36 @@
 import React from "react";
 import getRandom from "../helpers/getRandom";
+import Anniversaries from "./HolidayBanners/Anniversaries";
+import Christmas from "./HolidayBanners/Christmas";
+import MLAnniversaries from "./HolidayBanners/MLAnniversaries";
+import Valentine from "./HolidayBanners/Valentine";
+import WhiteDays from "./HolidayBanners/WhiteDays";
 import { miniCharas } from "../helpers/miniCharas";
+import useVisualMode from "../hooks/useVisualHook";
 
 
 export default function Welcome(props) {
+  const NORMAL = 'NORMAL';
+  const ANNIVERSARY = 'ANNIVERSARY';
+  const CHRISTMAS = 'CHRISTMAS';
+  const MLANNIVERSARY = 'MLANNIVERSARY';
+  const VALENTINE = 'VALENTINE';
+  const WHITEDAY = 'WHITRDAY';
+  const { mode } = useVisualMode(VALENTINE);
+
+
   const miniCharaImgs = Object.values(miniCharas)
+  
   return (
     <div className="main-container">
       {/* Holiday Banners */}
+      {mode === ANNIVERSARY && <Anniversaries />}
+      {mode === CHRISTMAS && <Christmas />}
+      {mode === MLANNIVERSARY && <MLAnniversaries />}
+      {mode === VALENTINE && <Valentine />}
+      {mode === WHITEDAY && <WhiteDays />}
+
+      {/* Welcome Page */}
       <p className="title">Welcome to MLTD Calculator</p>
       <img alt="random mini chara" src={miniCharaImgs[getRandom(39)]} />
     </div>
