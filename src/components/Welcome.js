2935586cc4014692f84b0f16999d358927e893c5
Bosi Chen <chenbosi.bosi@gmail.com>
2022-03-14 03:44:44 -0400

    add function for change holiday banner automatically

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -9,17 +9,39 @@ import { miniCharas } from "../helpers/miniCharas";
 import useVisualMode from "../hooks/useVisualHook";
 
 
-export default function Welcome(props) {
+export default function Welcome() {
   const NORMAL = 'NORMAL';
+  const VALENTINE = 'VALENTINE';
+  const MLANNIVERSARY = 'MLANNIVERSARY';
+  const WHITEDAY = 'WHITRDAY';
   const ANNIVERSARY = 'ANNIVERSARY';
   const CHRISTMAS = 'CHRISTMAS';
-  const MLANNIVERSARY = 'MLANNIVERSARY';
-  const VALENTINE = 'VALENTINE';
-  const WHITEDAY = 'WHITRDAY';
-  const { mode } = useVisualMode(WHITEDAY);
 
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+
+  const setMode = (month, day) => {
+    if (month === 2 && day <= 20) {
+      return VALENTINE;
+    } else if (month === 2 && day > 20) {
+      return MLANNIVERSARY;
+    } else if (month === 3 && day < 20) {
+      return WHITEDAY;
+    } else if ((month === 6 && day >= 25) || month === 7 && day < 20) {
+      return ANNIVERSARY;
+    } else if (month === 12 && day >= 20) {
+      return CHRISTMAS;
+    } else {
+      return NORMAL;
+    };
+  };
+
+  const currentMode = setMode(month, day);
+  const { mode } = useVisualMode(currentMode);
 
   const miniCharaImgs = Object.values(miniCharas);
+  
   
   return (
     <div className="main-container">
