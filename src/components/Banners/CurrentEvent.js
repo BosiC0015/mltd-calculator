584964c4308933a936073f0fe599009d72aac79b
Bosi Chen <chenbosi.bosi@gmail.com>
2022-04-12 23:09:54 -0400

    event update

--- src/components/Banners/CurrentEvent.js
+++ src/components/Banners/CurrentEvent.js
@@ -5,13 +5,13 @@ import "./banners.scss";
 export default function CurrentEvent() {
   return (
     <div>
-      <p>Current Event: PSTune: Omide wa Cleasky</p>
-      <div className="event-essentials">
+      <p>Current Event: MillionLive Collection</p>
+      {/* <div className="event-essentials">
         <p>🔺Ranking SR: Miya</p>
         <p>🔸Point SR: Elena</p>
         <p>🔹Participating: /</p>
-      </div>
-      <img className="banner" src="https://pbs.twimg.com/media/FPJfRonaUAEIcfR?format=jpg&name=medium" alt="event" />
+      </div> */}
+      <img className="banner" src="https://pbs.twimg.com/media/FPy7UNnVEAc4brB?format=jpg&name=medium" alt="event" />
     </div>
   )
 }
