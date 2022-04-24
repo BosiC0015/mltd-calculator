bd329b1ea7d4b13f7aec6e4b4ad3531feaaa0be1
Bosi Chen <chenbosi.bosi@gmail.com>
2022-04-23 23:06:42 -0400

    complete work times calculation

--- src/components/Tale.js
+++ src/components/Tale.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { getDifference } from "../helpers/calculators";
-import Results from "./Results/Result";
+import TaleResults from "./Results/TaleResults";
 import "./styles.scss";
 
 
 export default function Tale(props) {
   const [currentPoint, setCurrentPoint] = useState('');
   const [targetPoint, setTargetPoint] = useState('');
-  const [stamina, setStamina] = useState(30);
+  const [stamina, setStamina] = useState(15);
   const [loadResults, setLoadResults] = useState(false);
 
   // calculation
@@ -25,13 +25,23 @@ export default function Tale(props) {
   const getWorkTimes = (stamina, eventSongPlays) => {
     if (stamina === 30) {
       return eventSongPlays * 6;
-    }
-  }
+    } else if (stamina === 25) {
+      return eventSongPlays * 7.5;
+    } else if (stamina === 20) {
+      return eventSongPlays * 10;
+    } else if (stamina === 15) {
+      return eventSongPlays * 15
+    };
+  };
+  const workTimes = getWorkTimes(stamina, eventSongPlays);
+
+  console.log(workTimes)
 
   // set states to default when click on Clear
   const clear = () => {
     setCurrentPoint('');
     setTargetPoint('');
+    setStamina(15);
     setLoadResults(false);
   };
 
@@ -70,10 +80,11 @@ export default function Tale(props) {
             onChange={(event) => setStamina(event.target.value)}
           >{ selectStamina }</select>
         </div>
-        <Results
+        <TaleResults
           load={loadResults}
           difference={difference}
           eventSongPlays={eventSongPlays}
+          workTimes={workTimes}
         />
         <div className="buttons">
           <button className="buttons-clear" onClick={clear}>Clear</button>
