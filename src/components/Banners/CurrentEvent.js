d74a2a3423329631596aa2ad0b0aaf1a1e62dd5a
Bosi Chen <chenbosi.bosi@gmail.com>
2022-04-27 10:01:24 -0400

    event update

--- src/components/Banners/CurrentEvent.js
+++ src/components/Banners/CurrentEvent.js
@@ -7,13 +7,13 @@ export default function CurrentEvent() {
   return (
     <div>
       <p>Current Event: MillionLive Collection</p>
-      <Countdown date="2022-04-25T20:59:59.000+09:00" />
-      <div className="event-essentials">
+      <Countdown date="2022-04-30T23:59:59.000+09:00" />
+      {/* <div className="event-essentials">
         <p>🔺Ranking SR: Noriko</p>
         <p>🔸Point SR: Arisa</p>
         <p>🔹Participating: Chihaya, Sayoko, Reika</p>
-      </div>
-      <img className="banner" src="https://pbs.twimg.com/media/FQHjOEjakAYSfjo?format=jpg&name=medium" alt="event" />
+      </div> */}
+      <img className="banner" src="https://pbs.twimg.com/media/FRKPFb-UcAEB-sI?format=png&name=900x900" alt="event" />
     </div>
   )
 }
