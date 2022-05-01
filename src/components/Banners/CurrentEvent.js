22b81a64378a36688b1210c8bc6559c302062a3d
Bosi Chen <chenbosi.bosi@gmail.com>
2022-05-01 02:19:07 -0400

    update new event

--- src/components/Banners/CurrentEvent.js
+++ src/components/Banners/CurrentEvent.js
@@ -6,14 +6,16 @@ import "./banners.scss";
 export default function CurrentEvent() {
   return (
     <div>
-      <p>Current Event: MillionLive Collection</p>
-      <Countdown date="2022-04-30T23:59:59.000+09:00" />
-      {/* <div className="event-essentials">
-        <p>🔺Ranking SR: Noriko</p>
-        <p>🔸Point SR: Arisa</p>
-        <p>🔹Participating: Chihaya, Sayoko, Reika</p>
-      </div> */}
-      <img className="banner" src="https://pbs.twimg.com/media/FRKPFb-UcAEB-sI?format=png&name=900x900" alt="event" />
+      {/* <p>Current Event: Platinum Star Theater: </p> */}
+      {/* <p>Current Event: Millionlive Collection</p> */}
+      <p>Current Event: Platinum Star Tour: CHEER UP! HEARTS UP!</p>
+      <Countdown date="2022-05-08T20:59:59.000+09:00" />
+      <div className="event-essentials">
+        <p>🔺Ranking SR: Anna</p>
+        <p>🔸Point SR: Kana</p>
+        <p>🔹Participating: Julia, Haruka</p>
+      </div>
+      <img className="banner" src="https://pbs.twimg.com/media/FRZpGx0VcAAMlMv?format=png&name=900x900" alt="event" />
     </div>
   )
 }
