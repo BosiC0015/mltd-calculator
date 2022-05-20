69d5ad63b7500a7b45bbaafeb342090d9a91c3d5
Bosi Chen <chenbosi.bosi@gmail.com>
2022-05-19 22:53:40 -0400

    event update

--- src/components/CurrentEvent/CurrentEvent.js
+++ src/components/CurrentEvent/CurrentEvent.js
@@ -15,18 +15,17 @@ export default function CurrentEvent() {
 
   return (
     <div>      
-      {/* <p>Current Event: Platinum Star Theater: </p> */}
-      <p>Current Event: Millionlive Collection</p>
-      {/* <p>Current Event: Platinum Star Tour: CHEER UP! HEARTS UP!</p> */}
-      <p>Time Remaining: <Countdown date="2022-05-15T20:59:59.000+09:00" /></p>
+      {/* <p>Current Event: Millionlive Collection</p> */}
+      <p>Current Event: Platinum Star Treasure: 空色♡ Birthday Card</p>
+      <p>Time Remaining: <Countdown date="2022-05-25T20:59:59.000+09:00" /></p>
       <div className="event-essentials">
-        {/* <p>🔺Ranking SR: Anna</p>
-        <p>🔸Point SR: Kana</p>
-        <p>🔹Participating: Julia, Haruka</p> */}
-        <p>🔸SR: Miki, Shiho</p>
-        <p>🔹R: Kotoha</p>
+        <p>🔺Ranking SR: Julia</p>
+        <p>🔸Point SR: Sayoko</p>
+        {/* <p>🔹Participating: Julia, Haruka</p> */}
+        {/* <p>🔸SR: Miki, Shiho</p>
+        <p>🔹R: Kotoha</p> */}
       </div>
-      <img className="banner" src="https://pbs.twimg.com/media/FRaxgmoVUAAu-Qt?format=png&name=900x900" alt="event" />
+      <img className="banner" src="https://pbs.twimg.com/media/FSmydeBUEAAdT2u?format=png&name=900x900" alt="event" />
 
       {mode === PST && <PSTEvent />}
       {mode === MILLICOLLE && <MilliColle />}
