a0091fc5638582b07fba3647f9e89f87386433c0
Bosi Chen <chenbosi.bosi@gmail.com>
2022-10-19 01:04:28 -0400

    event update

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -9,9 +9,9 @@ export default function PSTEvent() {
       <p>Current Event: Platinum Star Trust SP: ようこそミスティック・セレモニー</p>
         <p>Time Remaining: <Countdown date="2022-10-25T20:59:59.000+09:00" /></p>
         <div className="event-essentials-pst">
-          <p>🔺Ranking SR: Roco</p>
-          <p>🔸Point SR: Iku</p>
-          <p>🔹Participating: Elena</p>
+          <p>🔺Ranking SR: Kana</p>
+          <p>🔸Point SR: Tsubasa</p>
+          <p>🔹Participating: Mami, Reika, Tomoka</p>
         </div>
         <img className="banner" alt="event" src="https://pbs.twimg.com/media/Fe7HVnTVEAA6Imo?format=png&name=900x900" />
     </div>
