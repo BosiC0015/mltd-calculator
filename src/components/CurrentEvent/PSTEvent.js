0fc46571a174278d5379afa839e3bb3e21145fdf
Bosi Chen <chenbosi.bosi@gmail.com>
2022-10-19 00:44:07 -0400

    event update

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -6,14 +6,14 @@ import "../Banners/banners.scss";
 export default function PSTEvent() {
   return (
     <div>
-      <p>Current Event: Platinum Star Theater: ゲキテキ！ムテキ！恋したい！</p>
-        <p>Time Remaining: <Countdown date="2022-10-09T20:59:59.000+09:00" /></p>
+      <p>Current Event: Platinum Star Trust SP: ようこそミスティック・セレモニー</p>
+        <p>Time Remaining: <Countdown date="2022-10-25T20:59:59.000+09:00" /></p>
         <div className="event-essentials-pst">
           <p>🔺Ranking SR: Roco</p>
           <p>🔸Point SR: Iku</p>
           <p>🔹Participating: Elena</p>
         </div>
-        <img className="banner" alt="event" src="https://pbs.twimg.com/media/FdzftT0UcAAZ_7Y?format=png&name=900x900" />
+        <img className="banner" alt="event" src="https://pbs.twimg.com/media/Fe7HVnTVEAA6Imo?format=png&name=900x900" />
     </div>
   );
 };
