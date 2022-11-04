abf9aae31ac441b38d9eaeb49a0318fa6d2e6a3f
Bosi Chen <chenbosi.bosi@gmail.com>
2022-11-04 18:17:56 -0400

    update event

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -6,14 +6,14 @@ import "../Banners/banners.scss";
 export default function PSTEvent() {
   return (
     <div>
-      <p>Current Event: Platinum Star Trust SP: ようこそミスティック・セレモニー</p>
-        <p>Time Remaining: <Countdown date="2022-10-25T20:59:59.000+09:00" /></p>
+      <p>Current Event: Platinum Star Theater: ハルカナミライ</p>
+        <p>Time Remaining: <Countdown date="2022-11-08T20:59:59.000+09:00" /></p>
         <div className="event-essentials-pst">
-          <p>🔺Ranking SR: Kana</p>
-          <p>🔸Point SR: Tsubasa</p>
-          <p>🔹Participating: Mami, Reika, Tomoka</p>
+          <p>🔺Ranking SR: Haruka</p>
+          <p>🔸Point SR: Mirai</p>
+          <p>🔹Participating: /</p>
         </div>
-        <img className="banner" alt="event" src="https://pbs.twimg.com/media/Fe7HVnTVEAA6Imo?format=png&name=900x900" />
+        <img className="banner" alt="event" src="https://pbs.twimg.com/media/FgIkmg9UUAAo7k9?format=png&name=900x900" />
     </div>
   );
 };
