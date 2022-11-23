47c85d3e71ae1d40782f130bb10811b614202abc
Bosi Chen <chenbosi.bosi@gmail.com>
2022-11-22 19:39:41 -0500

    event update

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -7,13 +7,13 @@ export default function PSTEvent() {
   return (
     <div>
       <p>Current Event: Platinum Star Theater: ハルカナミライ</p>
-        <p>Time Remaining: <Countdown date="2022-11-08T20:59:59.000+09:00" /></p>
+        <p>Time Remaining: <Countdown date="2022-11-24T20:59:59.000+09:00" /></p>
         <div className="event-essentials-pst">
-          <p>🔺Ranking SR: Haruka</p>
-          <p>🔸Point SR: Mirai</p>
-          <p>🔹Participating: /</p>
+          <p>🔺Ranking SR: Kotoha</p>
+          <p>🔸Point SR: ROCO</p>
+          <p>🔹Participating: Yukiho, Makoto, Iori, Tsubasa, Matsuri, Serika, Nao, Chizuru, Tamaki, Miya, Momoko</p>
         </div>
-        <img className="banner" alt="event" src="https://pbs.twimg.com/media/FgIkmg9UUAAo7k9?format=png&name=900x900" />
+        <img className="banner" alt="event" src="https://pbs.twimg.com/media/FhCYEvYaMAAVsUY?format=png&name=900x900" />
     </div>
   );
 };
