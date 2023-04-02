7be8025d1ea3ea24cae4575c922ba925cc1e1692
Bosi Chen <chenbosi.bosi@gmail.com>
2023-04-02 02:19:03 -0400

    event update

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -6,14 +6,19 @@ import "../Banners/banners.scss";
 export default function PSTEvent() {
   return (
     <div>
-      <p>Current Event: Platinum Star Theater: Sweet Sweet Soul</p>
-        <p>Time Remaining: <Countdown date="2023-03-9T20:59:59.000+09:00" /></p>
+      <p className="event-title">Current Event</p>
+      <p className="event-title">Platinum Star Theater: Cut. Cut. Cut.</p>
+        <p>Time Remaining: <Countdown date="2023-04-10T20:59:59.000+09:00" /></p>
         <div className="event-essentials-pst">
-          <p>🔺Ranking SR: Akane</p>
-          <p>🔸Point SR: Tamaki</p>
-          <p>🔹Participating: Serika</p>
+          <p>🔺Ranking SR: Mizuki</p>
+          <p>🔸Point SR: Momoko</p>
+          <p>🔹Participating: /</p>
         </div>
-        <img className="banner" alt="event" src="https://pbs.twimg.com/media/Fp8veEfaMAAhtCG?format=png&name=900x900" />
+        <img 
+          className="banner" 
+          alt="event" 
+          src="https://pbs.twimg.com/media/Fsg3lHdacAA_7A4?format=png&name=900x900" 
+        />
     </div>
   );
 };
