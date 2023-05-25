4813e3838721130fb31ceeb15042939835f380db
Bosi Chen <chenbosi.bosi@gmail.com>
2023-05-25 18:14:28 -0400

    event update

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -7,17 +7,17 @@ export default function PSTEvent() {
   return (
     <div>
       <p className="event-title">Current Event</p>
-      <p className="event-title">Platinum Star Theater: Cut. Cut. Cut.</p>
-        <p>Time Remaining: <Countdown date="2023-04-10T20:59:59.000+09:00" /></p>
+      <p className="event-title">Platinum Star Tour: 電波感傷</p>
+        <p>Time Remaining: <Countdown date="2023-05-25T20:59:59.000+09:00" /></p>
         <div className="event-essentials-pst">
-          <p>🔺Ranking SR: Mizuki</p>
-          <p>🔸Point SR: Momoko</p>
+          <p>🔺Ranking SR: Kaori</p>
+          <p>🔸Point SR: Tsumugi</p>
           <p>🔹Participating: /</p>
         </div>
         <img 
           className="banner" 
           alt="event" 
-          src="https://pbs.twimg.com/media/Fsg3lHdacAA_7A4?format=png&name=900x900" 
+          src="https://pbs.twimg.com/media/FwUcyNmaEAAWg02?format=png&name=900x900" 
         />
     </div>
   );
