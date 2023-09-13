f324360a6ecbda039433cbdadbeede3ce4e5147a
Bosi Chen <chenbosi.bosi@gmail.com>
2023-09-13 19:50:48 -0400

    update event

--- src/components/CurrentEvent/CurrentEvent.js
+++ src/components/CurrentEvent/CurrentEvent.js
@@ -13,7 +13,7 @@ export default function CurrentEvent() {
   const TALKPARTY = 'TALKPARTY';
   const ANNIV = 'ANNIV';
 
-  const { mode } = useVisualMode(ANNIV);
+  const { mode } = useVisualMode(MILLICOLLE);
 
   return (
     <div>
--- src/components/CurrentEvent/MilliColle.js
+++ src/components/CurrentEvent/MilliColle.js
@@ -7,13 +7,13 @@ export default function MilliColle() {
   return (
     <div>
       <p>Current Event: Millionlive Collection</p>
-      <p>Time Remaining: <Countdown date="2022-05-25T20:59:59.000+09:00" /></p>
+      <p>Time Remaining: <Countdown date="2023-09-15T20:59:59.000+09:00" /></p>
       <div className="event-essentials">
-        <p>🔸SR1: </p>
-        <p>🔸SR2: </p>
-        <p>🔹R: </p>
+        <p>🔸SR1: Miki</p>
+        <p>🔸SR2: Rio</p>
+        <p>🔹R: Minako</p>
       </div>
-      <img className="banner" src="" alt="event" />
+      <img className="banner" src="https://pbs.twimg.com/media/F5Y9lhJbcAAp_yk?format=png&name=900x900" alt="event" />
     </div>
   );
 };
