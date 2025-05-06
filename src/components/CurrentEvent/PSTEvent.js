237cb72c743370124a4c42f4cb73a40b1917b4a8
Bosi Chen <chenbosi.bosi@gmail.com>
2025-05-06 13:44:19 -0400

    event update

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -7,17 +7,17 @@ export default function PSTEvent() {
   return (
     <div className="event">
       {/* <p className="event-current">CURRENT EVENT</p> */}
-      <p className="event-title">Platinum Star Team : Unknown Boxの開き方</p>
-        <p className="event-time">Time Remaining: <Countdown date="2024-03-08T20:59:59.000+09:00" /></p>
+      <p className="event-title">Platinum Star Theater : Fu-Wa-Du-Wa</p>
+        <p className="event-time">Time Remaining: <Countdown date="2025-05-08T20:59:59.000+09:00" /></p>
         <div className="event-essentials">
-          <p>🔺Ranking SR: Matsuri</p>
-          <p>🔸Point SR: Elena</p>
-          <p>🔹Participating: Hinata, Emily, Subaru</p>
+          <p>🔺Ranking SR: Mami</p>
+          <p>🔸Point SR: Ayumu</p>
+          <p>🔹Participating: Elena, Subaru</p>
         </div>
         <img 
           className="banner" 
           alt="event" 
-          src="https://pbs.twimg.com/media/GHY_UzZbgAAWmT3?format=png&name=900x900" 
+          src="https://pbs.twimg.com/media/GpXfWqEbcAAALTb?format=png&name=900x900" 
         />
     </div>
   );
