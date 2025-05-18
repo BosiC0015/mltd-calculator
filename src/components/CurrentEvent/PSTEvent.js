0eca84d3005c1cfc6eb6551510a8ae461e99539d
Bosi Chen <chenbosi.bosi@gmail.com>
2025-05-18 14:13:46 -0400

    update event

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -6,18 +6,18 @@ import "../Banners/banners.scss";
 export default function PSTEvent() {
   return (
     <div className="event">
-      {/* <p className="event-current">CURRENT EVENT</p> */}
-      <p className="event-title">Platinum Star Theater : Fu-Wa-Du-Wa</p>
-        <p className="event-time">Time Remaining: <Countdown date="2025-05-08T20:59:59.000+09:00" /></p>
-        <div className="event-essentials">
-          <p>🔺Ranking SR: Mami</p>
-          <p>🔸Point SR: Ayumu</p>
-          <p>🔹Participating: Elena, Subaru</p>
-        </div>
+      <p className="event-current">CURRENT EVENT</p>
+      <p className="event-title">Platinum Star Tour Bingo : Clash of Colors</p>
+        <p className="event-time">Time Remaining: <Countdown date="2025-05-25T20:59:59.000+09:00" /></p>
+        {/* <div className="event-essentials"> */}
+          {/* <p>🔺Ranking SR: Mami</p> */}
+          {/* <p>🔸Point SR: Ayumu</p> */}
+          {/* <p>🔹Participating: Elena, Subaru</p> */}
+        {/* </div> */}
         <img 
           className="banner" 
           alt="event" 
-          src="https://pbs.twimg.com/media/GpXfWqEbcAAALTb?format=png&name=900x900" 
+          src="https://pbs.twimg.com/media/Gq8_87FaMAA2mIX?format=png&name=900x900" 
         />
     </div>
   );
