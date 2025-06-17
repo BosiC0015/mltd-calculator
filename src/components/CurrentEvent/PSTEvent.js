13f3b79e9cb9a513216a481c4d74f17461d85d7f
Bosi Chen <chenbosi.bosi@gmail.com>
2025-06-16 21:56:09 -0400

    Update PSTEvent.js
    update event

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -7,8 +7,8 @@ export default function PSTEvent() {
   return (
     <div className="event">
       <p className="event-current">CURRENT EVENT</p>
-      <p className="event-title">Platinum Star Theater : U・N・M・E・I ライブ</p>
-        <p className="event-time">Time Remaining: <Countdown date="2025-06-07T20:59:59.000+09:00" /></p>
+      <p className="event-title">Platinum Tour Bingo : Luvliminal image</p>
+        <p className="event-time">Time Remaining: <Countdown date="2025-06-23T20:59:59.000+09:00" /></p>
         {/* <div className="event-essentials"> */}
           {/* <p>🔺Ranking SR: Mami</p> */}
           {/* <p>🔸Point SR: Ayumu</p> */}
@@ -17,7 +17,7 @@ export default function PSTEvent() {
         <img 
           className="banner" 
           alt="event" 
-          src="https://pbs.twimg.com/media/Gr3RO9zXUAE7Rtk?format=png&name=900x900" 
+          src="https://pbs.twimg.com/media/GtN-EGZbMAIcB9T?format=png&name=900x900" 
         />
     </div>
   );
