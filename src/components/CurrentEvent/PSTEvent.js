58ca05739316a343d4aa288f1484332b5e777cbb
Bosi Chen <chenbosi.bosi@gmail.com>
2025-08-22 18:06:05 -0400

    event update

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -2,16 +2,16 @@ import React from "react";
 import "../Banners/banners.scss";
 import "./event.scss";
 import CountdownContainer from "../Countdown";
-import CardShowcase from "../Card/CardShowcase";
+// import CardShowcase from "../Card/CardShowcase";
 
 
 export default function PSTEvent() {
   return (
     <div className="event">
       <p className="event-current">CURRENT EVENT</p>
-      <p className="event-title">Platinum Treasure : ザ・ライブ革命でＳＨＯＷ！</p>
+      <p className="event-title">Platinum Star Tour BINGO : SHIMMER</p>
       <p className="event-time">Time Remaining: </p>
-      <CountdownContainer endTime="2025-08-08T20:59:59.000+09:00" />
+      <CountdownContainer endTime="2025-08-25T20:59:59.000+09:00" />
       {/* <div className="event-essentials"> */}
         {/* <p>🔺Ranking SR: Mami</p> */}
         {/* <p>🔸Point SR: Ayumu</p> */}
@@ -20,7 +20,7 @@ export default function PSTEvent() {
       <img 
         className="banner" 
         alt="event" 
-        src="https://pbs.twimg.com/media/Gw_SnRKbUAAggJs?format=png&name=900x900" 
+        src="https://pbs.twimg.com/media/GxpeZtcawAMcVLE?format=png&name=large" 
       />
       {/* <CardShowcase 
         cardLink="https://cmsapi-frontend.idolmaster-official.jp/sitern/api/idolmaster/Image/get?path=/idolmaster/jp/article/1001/2025/07/XXRIJv6lhURh3QkgbftSxPx9np1Ypqzk.jpeg"
