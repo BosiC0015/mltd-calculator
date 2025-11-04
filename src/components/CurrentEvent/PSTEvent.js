6ebd94cec0fb15c28cbffb2fced5c84228c058ff
Bosi Chen <chenbosi.bosi@gmail.com>
2025-11-04 17:42:31 -0500

    event update

--- src/components/CurrentEvent/PSTEvent.js
+++ src/components/CurrentEvent/PSTEvent.js
@@ -9,9 +9,9 @@ export default function PSTEvent() {
   return (
     <div className="event">
       <p className="event-current">CURRENT EVENT</p>
-      <p className="event-title">Platinum Star Tour BINGO : 不思議発見ラボ！</p>
+      <p className="event-title"><b>プラチナスターシアター ～ Halftone ～</b></p>
       <p className="event-time">Time Remaining: </p>
-      <CountdownContainer endTime="2025-10-25T20:59:59.000+09:00" />
+      <CountdownContainer endTime="2025-11-07T20:59:59.000+09:00" />
       {/* <div className="event-essentials"> */}
         {/* <p>🔺Ranking SR: Mami</p> */}
         {/* <p>🔸Point SR: Ayumu</p> */}
@@ -20,7 +20,8 @@ export default function PSTEvent() {
       <img 
         className="banner" 
         alt="event" 
-        src="https://pbs.twimg.com/media/G3WsXf1a8AAFHce?format=png&name=large" 
+        // src="https://pbs.twimg.com/media/G3WsXf1a8AAFHce?format=png&name=large" 
+        src="https://cmsapi-frontend.idolmaster-official.jp/sitern/api/idolmaster/Image/get?path=/idolmaster/jp/article/1001/2025/10/EYbDE4skbnwhFGZJu1pWd8X0gfR3qDGI.jpeg"
       />
       {/* <CardShowcase 
         cardLink="https://cmsapi-frontend.idolmaster-official.jp/sitern/api/idolmaster/Image/get?path=/idolmaster/jp/article/1001/2025/07/XXRIJv6lhURh3QkgbftSxPx9np1Ypqzk.jpeg"
