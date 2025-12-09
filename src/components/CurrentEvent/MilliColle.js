53bf843b8c8de4adadb1a7b94d12a924796066d2
Bosi Chen <chenbosi.bosi@gmail.com>
2025-12-09 14:26:43 -0500

    update event

--- src/components/CurrentEvent/MilliColle.js
+++ src/components/CurrentEvent/MilliColle.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import Countdown from "react-countdown";
 import "../Banners/banners.scss";
+import CountdownContainer from "../Countdown";
 
 
 export default function MilliColle() {
@@ -8,12 +8,13 @@ export default function MilliColle() {
     <div>
       <p className="event-title">Current Event: Millionlive Collection</p>
       <p className="event-time">Time Remaining: </p>
+      <CountdownContainer endTime="2025-12-14T20:59:59.000+09:00" />
       <div className="event-essentials">
         {/* <p>🔸SR 1: Miki</p> */}
         {/* <p>🔸SR 2: Rio</p> */}
         {/* <p>🔹R: Minako</p> */}
       </div>
-      <img className="banner" src="https://pbs.twimg.com/media/F5Y9lhJbcAAp_yk?format=png&name=900x900" alt="event" />
+      <img className="banner" src="https://pbs.twimg.com/media/G7Slgz9agAADY6O?format=jpg&name=large" alt="event" />
     </div>
   );
 };
