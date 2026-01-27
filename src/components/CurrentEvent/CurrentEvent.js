152e9866f9158ff460c73f3386632931764151ca
Bosi Chen <chenbosi.bosi@gmail.com>
2026-01-27 11:17:39 -0500

    update event

--- src/components/CurrentEvent/CurrentEvent.js
+++ src/components/CurrentEvent/CurrentEvent.js
@@ -13,7 +13,7 @@ export default function CurrentEvent() {
   const TALKPARTY = 'TALKPARTY';
   const ANNIV = 'ANNIV';
 
-  const { mode } = useVisualMode(PST);
+  const { mode } = useVisualMode(TALKPARTY);
 
   return (
     <div>
--- src/components/CurrentEvent/TalkParty.js
+++ src/components/CurrentEvent/TalkParty.js
@@ -1,16 +1,17 @@
 import React from "react";
-// import Countdown from "react-countdown";
-import "../Banners/banners.scss";
 import CountdownContainer from "../Countdown";
+import "../Banners/banners.scss";
+import "./event.scss";
 
 
 export default function TalkParty() {
   return (
-    <div>
-      <p>Current Event: Million Live TalkParty</p>
-        <p>Time Remaining: </p>
-        <CountdownContainer endTime="2022-05-25T23:59:59.000+09:00" />
-        <img className="banner" src="" alt="event" />
+    <div className="event">
+      {/* <p>Current Event: Million Live TalkParty</p> */}
+      <p className="event-title"><b>THEATER TALK PARTY☆<br></br>～ユニットオフショット Vol.59～</b></p>
+      <p>Time Remaining: </p>
+      <CountdownContainer endTime="2026-01-30T23:59:59.000+09:00" />
+      <img className="banner" src="https://pbs.twimg.com/media/G_OsNLrboAAjldl?format=jpg&name=large" alt="talk-party-event" />
     </div>
   );
 };
