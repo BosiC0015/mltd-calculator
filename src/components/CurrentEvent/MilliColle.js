8e23ac5c3055b2ee137115462f5b8b04b22e9af7
Bosi Chen <chenbosi.bosi@gmail.com>
2026-02-09 17:35:26 -0500

    update event

--- src/components/CurrentEvent/MilliColle.js
+++ src/components/CurrentEvent/MilliColle.js
@@ -8,13 +8,13 @@ export default function MilliColle() {
     <div>
       <p className="event-title">Current Event: Millionlive Collection</p>
       <p className="event-time">Time Remaining: </p>
-      <CountdownContainer endTime="2026-01-16T20:59:59.000+09:00" />
+      <CountdownContainer endTime="2026-02-14T20:59:59.000+09:00" />
       <div className="event-essentials">
         {/* <p>🔸SR 1: Miki</p> */}
         {/* <p>🔸SR 2: Rio</p> */}
         {/* <p>🔹R: Minako</p> */}
       </div>
-      <img className="banner" src="https://pbs.twimg.com/media/G-C3rqWbUAA2XDV?format=jpg&name=large" alt="event" />
+      <img className="banner" src="https://pbs.twimg.com/media/HAX56AWagAAHavE?format=jpg&name=large" alt="event" />
     </div>
   );
 };
