500e7f93f29957ffcabbe75b80e3b4842e74df38
Bosi Chen <chenbosi.bosi@gmail.com>
2026-04-18 17:12:01 -0400

    update event

--- src/components/CurrentEvent/PSTEvent.jsx
+++ src/components/CurrentEvent/PSTEvent.jsx
@@ -8,16 +8,16 @@ export default function PSTEvent() {
   return (
     <div className="event">
       <p className="event-title" lang="jp">
-        プラチナスターチューン<br></br>
-        ～ パンとフィルム ～
+        プラチナスタートラストBOT<br></br>
+        ～ Stick to my weapon ～
       </p>
       <p className="event-title en-alt">
-        Platinum Star Tune<br></br>
-        ～ パンとフィルム ～
+        Platinum Star Trust BOT (BATTLE OF THEATER)<br></br>
+        ～ Stick to my weapon ～
       </p>
       <div className="countdown">
         <p className="event-time">Time Remaining: </p>
-        <CountdownContainer endTime="2026-04-09T20:59:59.000+09:00" />
+        <CountdownContainer endTime="2026-04-25T20:59:59.000+09:00" />
       </div>
       <img className="banner" alt="event" src={currentEventBanner} />
     </div>
