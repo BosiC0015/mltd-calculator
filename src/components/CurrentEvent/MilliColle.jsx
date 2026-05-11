2b6f2fa6c8d4ae3392626aa85d12c380b8b3e098
Bosi Chen <chenbosi.bosi@gmail.com>
2026-05-11 17:20:46 -0400

    update event

--- src/components/CurrentEvent/MilliColle.jsx
+++ src/components/CurrentEvent/MilliColle.jsx
@@ -7,12 +7,12 @@ import "../Banners/banners.scss";
 export default function MilliColle() {
   return (
     <div id="millicolle" className="event">
-      <img className="banner" alt="event" src={currentEventBanner} />
       <p className="event-title">Current Event: MILLIONLIVE COLLECTION</p>
       <div className="countdown">
         <p className="event-time">Time Remaining: </p>
-        <CountdownContainer endTime="2026-04-16T20:59:59.000+09:00" />
+        <CountdownContainer endTime="2026-05-16T20:59:59.000+09:00" />
       </div>
+      <img className="banner" alt="event" src={currentEventBanner} />
     </div>
   );
 };
