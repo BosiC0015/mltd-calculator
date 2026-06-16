ff43a3f45bd0a3da25219a716354693959c6f013
Bosi Chen <chenbosi.bosi@gmail.com>
2026-06-16 15:53:02 -0400

    update event

--- src/components/CurrentEvent/PSTEvent.jsx
+++ src/components/CurrentEvent/PSTEvent.jsx
@@ -9,15 +9,15 @@ export default function PSTEvent() {
     <div className="event">
       <p className="event-title" lang="jp">
         プラチナスターツアービンゴ<br></br>
-        ～100てん☆ナンバーワン！～
+        ～ INVINCIBLE LIGHT ～
       </p>
       <p className="event-title en-alt">
         Platinum Star Tour BINGO<br></br>
-        ～ 100 points ☆ number one！ ～
+        ～ INVINCIBLE LIGHT ～
       </p>
       <div className="countdown">
         <p className="event-time">Time Remaining: </p>
-        <CountdownContainer endTime="2026-05-25T20:59:59.000+09:00" />
+        <CountdownContainer endTime="2026-06-23T20:59:59.000+09:00" />
       </div>
       <img className="banner" alt="event" src={currentEventBanner} />
     </div>
