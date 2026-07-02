4aec1ba9577af915051323c93f00ef7e41c47501
BosiC0015 <chenbosi.bosi@gmail.com>
2026-07-02 00:06:11 -0400

    update event

--- src/components/CurrentEvent/AnniversaryEvent.jsx
+++ src/components/CurrentEvent/AnniversaryEvent.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import "../Banners/banners.scss";
+import currentEventBanner from "../../assets/images/event_banner/current.jpeg"
 import CountdownContainer from "../Countdown";
+import "./event.scss";
 
 
 export default function AnnivEvent() {
   return (
     <div className="event">
-      <p className="event-title">8th Anniversary Event: 超々∞MUGEND@I!</p>
-        <p className="event-time">Time Remaining: </p>
-          <CountdownContainer endTime="2025-07-12T20:59:59.000+09:00" />
-        <img 
-          className="banner" 
-          alt="event" 
-          src="https://pbs.twimg.com/media/Gub4RoNa0AAudnW?format=png&name=900x900" 
-        />
-    </div>
+          <p className="event-title" lang="jp">9周年記念イベント『最上級：Invit@tion!』</p>
+          <div className="countdown">
+            <p className="event-time">Time Remaining: </p>
+            <CountdownContainer endTime="2026-07-12T23:59:59.000+09:00" />
+          </div>
+          <img className="banner" alt="event" src={currentEventBanner} />
+        </div>
   );
 };
