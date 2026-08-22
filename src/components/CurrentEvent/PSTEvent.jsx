db645679184f33586200b5d01135e9a27b904bbe
Bosi Chen <chenbosi.bosi@gmail.com>
2026-08-21 23:21:13 -0400

    update event

--- src/components/CurrentEvent/PSTEvent.jsx
+++ src/components/CurrentEvent/PSTEvent.jsx
@@ -7,17 +7,17 @@ import "./event.scss";
 export default function PSTEvent() {
   return (
     <div className="event">
-      <p className="event-title" lang="jp">
-        プラチナスタートレジャー<br></br>
-        ～ Destiny ～
+      <p className="event-title" lang="ja">
+        プラチナスタートラストBOT<br></br>
+        ～ Glow Together ～
       </p>
       <p className="event-title en-alt">
-        Platinum Star Treasure<br></br>
-        ～ Destiny ～
+        Platinum Star Trust BOT<br></br>
+        ～ Glow Together ～
       </p>
       <div className="countdown">
         <p className="event-time">Time Remaining: </p>
-        <CountdownContainer endTime="2026-08-09T20:59:59.000+09:00" />
+        <CountdownContainer endTime="2026-08-25T20:59:59.000+09:00" />
       </div>
       <img className="banner" alt="event" src={currentEventBanner} />
     </div>
